4810f54e53810fa60282b8cdb5d9a74dd7b2fd51
Sokus <soqs687@gmail.com>
2022-08-24 18:55:26 +0200

    nuke

---
+++ procyon_app.h
@@ -0,0 +1,18 @@
+#ifndef PROCYON_APP_H
+#define PROCYON_APP_H
+
+void procyon_app_init();
+
+#endif // PROCYON_APP_H
+
+
+#ifdef PROCYON_APP_IMPLEMENTATION
+
+#include <stdio.h>
+
+void procyon_app_init()
+{
+    printf("procyon_app_init()\n");
+}
+
+#endif // PROCYON_APP_IMPLEMENTATION
